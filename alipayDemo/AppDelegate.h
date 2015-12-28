263112c83a36b91e082e7751739a1d542ae9328d
zhangmingjia <zhangmingjia@juxinli.com>
2015-12-28 14:12:44 +0800

    init

---
+++ alipayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  alipayDemo
+//
+//  Created by maxbalance on 12/28/15.
+//  Copyright © 2015 maxbalance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ alipayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  alipayDemo
+//
+//  Created by maxbalance on 12/28/15.
+//  Copyright © 2015 maxbalance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
